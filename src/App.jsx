e2ef2757d953aacf6dbf586c055ea143208e3e87
Calisto-Mathias <calisto.mathias.25@gmail.com>
2024-03-10 01:25:26 +0530

    Web-App

---
+++ src/App.jsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import "./App.css";
+import upload from "./upload.png";
+
+function App() {
+  const [file, setFile] = useState();
+  const [response, setResponse] = useState("");
+
+  const URL = "";
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await axios.post(URL, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    if (response) {
+      setResponse(response.data.caption);
+    }
+  };
+
+  return (
+    <>
+      <div className="h-screen w-screen background-image relative overflow-x-hidden">
+        <nav className="w-screen h-[15vh] fixed flex lg:justify-between justify-center items-center px-[100px] bg-[rgb(0,0,0,0.5)]">
+          <ul className="hidden lg:list-none lg:flex lg:justify-content lg:items-center lg:gap-10">
+            <li>
+              <a
+                href=""
+                className="text-white hover:text-[#51ED9E] duration-200"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#upload"
+                className="text-white hover:text-[#51ED9E] duration-200"
+              >
+                Upload
+              </a>
+            </li>
+            <li>
+              <div className="text-white">#Sensing</div>
+            </li>
+          </ul>
+          <h1 className="courgette-regular text-[2rem] text-white">SkyWatch</h1>
+          <div className="hidden lg:flex lg:flex-row lg:justify-evenly lg:items-center lg:gap-10">
+            <a
+              href="#upload"
+              className="rounded px-5 py-3 border-rounded border-[#51ED9E] border-[0.5px] text-[#51ED9E] font-semibold hover:bg-[#51ED9E] hover:text-black duration-200"
+            >
+              Know More
+            </a>
+            <a
+              href="#upload"
+              className="rounded bg-[#51ED9E] px-5 py-3 font-semibold hover:scale-110 duration-200"
+            >
+              Get Started
+            </a>
+          </div>
+        </nav>
+        <div className="h-full w-full bg-gradient-to-b from-transparent to-[#000000]">
+          <div className="flex flex-col justify-center items-center h-full w-full gap-20">
+            <h1 className="lg:text-[3.5rem] w-[92.5%] text-[2rem] md:text-[2.5rem] font-semibold text-white text-center">
+              Driving Remote Sensing{" "}
+              <span className="rounded border-2 border-white px-2 py-1">
+                Eons
+              </span>{" "}
+              Ahead!
+            </h1>
+            <div className="w-[75%] flex flex-row justify-evenly items-center">
+              <div className="hidden lg:flex bg-gray-300 h-[1.75px] w-[35%]"></div>
+              <a
+                href="#upload"
+                className="rounded bg-[#51ED9E] px-5 py-3 font-semibold hover:scale-110 duration-200 lg:text-md text-sm"
+              >
+                Upload Your Image Now!
+              </a>
+              <div className="hidden lg:flex bg-gray-300 h-[1.75px] w-[35%]"></div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="h-screen w-screen bg-black flex flex-col gap-5 justify-center items-center"
+          id="upload"
+        >
+          <div className="border-2 border-zinc-300  w-[80%] lg:w-[50%] rounded flex flex-col justify-center items-center py-10 gap-[30px] lg:gap-5">
+            <h1 className="lg:text-[3rem] w-[92.5%] text-[2rem] md:text-[2.5rem] font-semibold text-white text-center">
+              Upload A Photo {":)"}
+            </h1>
+            <img src={upload} alt="" width={"40%"} />
+            <form
+              action=""
+              className="flex flex-col justify-evenly items-center gap-5 w-[80%]"
+            >
+              <input
+                type="file"
+                accept=".jpg"
+                name="FileUpload"
+                id="FileUpload"
+                className="text-white py-3 px-5 border-2 border-zinc-600 rounded flex flex-row w-[80%]"
+                onChange={onInputChange}
+              />
+              <button
+                type="submit"
+                className="text-white rounded border-2 border-zinc-300 px-5 py-3"
+              >
+                Upload!
+              </button>
+            </form>
+          </div>
+          {response && (
+            <div className="border-2 border-zinc-300 w-[80%] lg:w-[50%] rounded flex flex-row justify-center items-center py-5 text-white text-center">
+              {response}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
